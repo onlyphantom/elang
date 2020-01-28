ffc2793182edb9bc1403f9704a406f149008ab67
onlyphantom <samuel@outlook.sg>
2020-01-28 20:15:35 +0700

    add model save

--- training/word2vec/generatefrombca_01.py
+++ training/word2vec/generatefrombca_01.py
@@ -1,3 +1,4 @@
+import os
 from gensim.models import Word2Vec
 from simple_preprocess.bcapara import corpus_bca
 
@@ -18,3 +19,21 @@ model = Word2Vec(
     iter=ITER)
 
 print(model.wv.most_similar("kartu"))
+
+# [('kredit', 0.5386360287666321), 
+# ('berbagai', 0.5347478985786438), 
+# ('sejalan', 0.502410888671875), 
+# ('tahapan', 0.49144789576530457), 
+# ('menerapkan', 0.41903647780418396), 
+# ('terkemuka', 0.3761982023715973), 
+# ('tahun', 0.3546695113182068), 
+# ('atm', 0.3403625190258026), 
+# ('nama', 0.2962668836116791)]
+
+save_dir = os.path.dirname(os.path.realpath(__file__)) + '/model/fin.model'
+print(save_dir)
+model.save(save_dir)
+
+# model.corpus_total_words -> 139
+# model.wv['bca'] # ndarray of length 10
+# model.wv.vocab['atm'].count -> 5
