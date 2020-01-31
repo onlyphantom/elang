fe5f16057eeb871d566112bb068ba8c2d1378ae9
onlyphantom <samuel@outlook.sg>
2020-01-31 16:59:44 +0700

    handle lowercase in stopwords

--- word2vec/utils/cleansing.py
+++ word2vec/utils/cleansing.py
@@ -8,7 +8,7 @@ stopwords = list(open(stopwords_dir).read().splitlines())
 
 def remove_stopwords_id(sentence):
     for w in stopwords:
-        sentence = re.sub(r'\b'+w+r'\b', '', sentence)
+        sentence = re.sub(r'\b'+w+r'\b', '', sentence.lower())
         sentence = re.sub(r'\s+', ' ', sentence).strip()
     return sentence
 
